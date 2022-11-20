5e1c0bc1268b1f36ff44f1e10ae902498f41b41f
unknown <xx06102002@gmail.com>
2022-11-20 16:28:21 -0500

    Primer Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/*Modificar el programa anterior para que, en lugar de leer 8 números, se puedan leer tantos números como se quiera hasta que llegue un cero.
+ El resultado es la suma de todos los números leídos.*/
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = 1; int suma = 0; int i = 1;
+
+        while(num != 0)
+        {
+            Console.WriteLine("Digite cualquier número");
+            num = int.Parse(Console.ReadLine());
+
+            suma = suma + num;            
+        } 
+        Console.WriteLine("Finalizado");
+        Console.WriteLine("El resultado de la suma de los números leídos es: "+suma);
+    }
+}
